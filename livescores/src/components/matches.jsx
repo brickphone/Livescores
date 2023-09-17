ac9d792a270c05d7b01a74fd444d7fdba8c5deab
brickphone <mattis.hutt@gmail.com>
2023-09-17 22:51:54 +0200

    stuff

--- livescores/src/components/matches.jsx
+++ livescores/src/components/matches.jsx
@@ -1,6 +1,12 @@
+import Skeleton from '@mui/material/Skeleton'
+import { useState } from 'react';
+
 /* eslint-disable react/prop-types */
 const Matches = (props) => {
-  
+  const [loading, setLoading] = useState(false);
+
+
+
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-start justify-center league-container px-16 hover:bg-slate-100" id="teams" style={{ position: 'relative' }}>
@@ -12,7 +18,11 @@ const Matches = (props) => {
             }
         </div>
         <div className=" flex justify-between items-center" id="hometeam">
-          <img className="w-18 h-18 flex-none" src={props.homeLogo}></img>
+          {
+            (props.homeLogo === undefined)
+            ? <Skeleton variant='circular' width={40} height={40}></Skeleton>
+            : <img className="w-18 h-18 flex-none" src={props.homeLogo}></img>
+          }
           <h2 className="pl-2 font-semibold w-32 text-left">{props.homeName}</h2>
           { // This should be removed / changed
             (props.homeScore < 1) 
--- livescores/src/components/navbar.jsx
+++ livescores/src/components/navbar.jsx
@@ -1,27 +1,79 @@
-import Navsvg from "../assets/navsvg";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaBars } from 'react-icons/fa'
+import { GrClose } from 'react-icons/gr'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const sidebarRef = useRef(null) // outside click reference point
 
-  const toggleSidebar = () => {
+  const toggleSidebar = (event) => {
+    event.stopPropagation(); // prevent sidebar from not opening
+    console.log("opening sidebar")
     setIsOpen(!isOpen);
+  }
+
+  const closeSidebar = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setIsOpen(false);
+      console.log("closing sidebar");
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", closeSidebar);
+
+    return () => {
+      document.removeEventListener("click", closeSidebar)
+    };
+  }, []);
+
+  const sidebarStyle = {
+    width: isOpen ? "300px" : "0", 
+    height: "100%", 
+    position: "fixed",
+    borderRadius: isOpen ? "0 20px 20px 0" : "0",
+    top: 0,
+    left: 0,
+    backgroundColor: "#f1f5f9",
+    overflowX: "hidden",
+    transition: "all 2.5s",
+    zIndex: 1,
+  };
+
+  // make the body appear "blurry"
+  const overlayStyle = {
+    display: isOpen ? "block" : "none",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    zIndex: 2,
   }
 
   return (
     <main>
-      <div className="flex custom-padding padding-left space-x-1">
-        <Navsvg onClick={toggleSidebar} />
-        <h1 className="font-bold">LIVESCORE</h1>
-        <span id="football-emoji">⚽</span>
-      </div>
-      <div className={`navbar-line mt-3 ${isOpen ? 'open' : ''}`}>
-        {/* Content of your sidebar */}
+      <div style={overlayStyle}>
+        <div style={sidebarStyle} ref={sidebarRef}>
         {isOpen && (
-          <div className="sidebar-content">
-            {/* Add your sidebar content here */}
+          <div className="sidebar-content flex flex-row">
+            <ul>
+              <GrClose className=""></GrClose>
+            </ul>
           </div>
         )}
+      </div>
+    </div>
+      
+      <div className="flex custom-padding padding-left space-x-1 items-center">
+        <div className="sidebar-button-container">
+          <FaBars className="sidebar-button text-xl" onClick={toggleSidebar} />
+        </div>
+        <h1 className="font-bold text-xl">LIVESCORE</h1>
+        <span id="football-emoji">⚽</span>
+      </div>
+      <div className="navbar-line mt-3">
       </div>
     </main>
   );
