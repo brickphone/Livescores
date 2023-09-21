5d61693b38d44884bb9bfb32d5c7bfd6bc49dc66
brickphone <mattis.hutt@gmail.com>
2023-09-21 23:20:04 +0200

    buttons

--- livescores/src/components/navbar.jsx
+++ livescores/src/components/navbar.jsx
@@ -2,13 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import { FaBars } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
 import { Drawer } from "@mui/material";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import Switch from '@mui/material/Switch';
-import { MdOutlineDarkMode } from "react-icons/md"
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
+import { MdOutlineDarkMode } from "react-icons/md";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -78,9 +75,11 @@ const Navbar = () => {
         </div>
         <h1 className="font-bold text-xl">LIVESCORE</h1>
         <span id="football-emoji">⚽</span>
+        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Login</button>
+        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Sign Up</button>
       </div>
-      <div className="navbar-line mt-3">
-      </div>
+
+      <div className="navbar-line mt-3"></div>
     </navbar>
   );
 }
